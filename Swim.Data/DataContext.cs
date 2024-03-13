bba31cb7ff1d630eeb117c3ec62ad6e6c05204f4
Your Name <you@example.com>
2024-03-13 21:53:07 +0200

    myCommit

--- Swim.Data/DataContext.cs
+++ Swim.Data/DataContext.cs
@@ -1,11 +1,13 @@
 ﻿
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Swim.Core.Entities;
 
 namespace Swim.Data
 {
     public class DataContext: DbContext
     {
+        private readonly IConfiguration _configuration;
         public DbSet<Lesson> Lessons { get; set; }
         public DbSet<Presence> Presences { get; set; }
         public DbSet<Student> Students { get; set; }
@@ -14,9 +16,14 @@ namespace Swim.Data
         public int StudentCount = 1;
         public int TeacherCount = 1;
 
+        public DataContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=myswim_db");
+            optionsBuilder.UseSqlServer(_configuration["ConnectionString"]);
         }
 
     }
